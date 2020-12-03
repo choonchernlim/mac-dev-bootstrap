78bd6e0f7d0d064e6a33e06c3ef74cf67418f0b1
Lim, Choon-Chern (Mike) <choonchernlim@gmail.com>
2020-12-02 22:16:35 -0600

    ohmyzsh

--- roles/ohmyzsh/files/ohmyzsh.sh
+++ roles/ohmyzsh/files/ohmyzsh.sh
@@ -1,17 +1,16 @@
-
 ZSH="${HOME}/.oh-my-zsh"
 ZSH_CUSTOM="${ZSH}/custom"
 ZSH_THEME="spaceship"
 
 if [[ -z "${TERM_PROGRAM}" || "${TERM_PROGRAM}" == "iTerm.app" ]]; then
-    export TERM="xterm-256color"
-    ZSH_THEME="powerlevel10k/powerlevel10k"
+  export   TERM="xterm-256color"
+  ZSH_THEME="powerlevel10k/powerlevel10k"
 fi
 
 plugins=(
-	git
-	zsh-syntax-highlighting
-	zsh-autosuggestions
+  git
+  zsh-syntax-highlighting
+  zsh-autosuggestions
 )
 
 source "${ZSH}/oh-my-zsh.sh"
