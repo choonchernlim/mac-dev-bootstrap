40cd19f8fd3ebe6077317261de1769d919a08b02
Lim, Choon-Chern (Mike) <choonchernlim@gmail.com>
2020-12-05 12:20:42 -0600

    gcloud

---
+++ roles/gcloud/files/gcloud.sh
@@ -0,0 +1,4 @@
+. "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+. "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
+gcloud --version
