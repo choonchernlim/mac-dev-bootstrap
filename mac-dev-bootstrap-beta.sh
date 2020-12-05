50c7c737a576005d651a104bde418e94dfc1e19c
Lim, Choon-Chern (Mike) <choonchernlim@gmail.com>
2020-12-05 11:13:44 -0600

    cleanup. bootstrap

--- mac-dev-bootstrap-beta.sh
+++ mac-dev-bootstrap-beta.sh
@@ -1,5 +1,12 @@
-# if brew don't exist,
-# - install homebrew
-# - brew install ansible python
+#!/usr/bin/env bash
+set -e -o pipefail
 
-# ansible-playbook main.yml
+# Install Homebrew if missing
+[[ ! -x "$(command -v brew)" ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install ansible and its dependencies
+[[ ! -x "$(command -v ansible)" ]] && brew install ansible
+[[ ! -x "$(command -v python)" ]] && brew install python
+
+# Run the playbook
+ansible-playbook main.yml
