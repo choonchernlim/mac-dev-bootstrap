bfaa96aba2e4989c0a841db0411921eb606eab39
Lim, Choon-Chern (Mike) <choonchernlim@gmail.com>
2020-12-05 12:53:12 -0600

    ansible-lint

--- mac-dev-bootstrap-beta.sh
+++ mac-dev-bootstrap-beta.sh
@@ -4,7 +4,8 @@ set -e -o pipefail
 # Install Homebrew if missing
 [[ ! -x "$(command -v brew)" ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Install ansible and its dependencies
+
+# Install Ansible and its dependencies
 [[ ! -x "$(command -v ansible)" ]] && brew install ansible
 [[ ! -x "$(command -v python)" ]] && brew install python
 
