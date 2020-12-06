a9ed2ad0e26d4f1d0a2b75fd2fe258309693c3a3
Lim, Choon-Chern (Mike) <choonchernlim@gmail.com>
2020-12-06 10:11:22 -0600

    Better log messages.

--- mac-dev-bootstrap.sh
+++ mac-dev-bootstrap.sh
@@ -2,6 +2,8 @@
 set -e -o pipefail
 
 readonly BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly LOG_DIR="${BASE_DIR}/logs"
+readonly LOG_PATH="${LOG_DIR}/ansible-$(date +%Y%m%d%H%M%S).log"
 
 # Install Homebrew if missing
 [[ ! -x "$(command -v brew)" ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -11,8 +13,11 @@ readonly BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 [[ ! -x "$(command -v ansible-lint)" ]] && brew install ansible-lint
 [[ ! -x "$(command -v python)" ]] && brew install python
 
+# Create log dir if not exist
+mkdir -p "${LOG_DIR}"
+
 # Run the playbook
-ANSIBLE_LOG_PATH="${BASE_DIR}/logs/ansible-$(date +%Y%m%d%H%M%S).log" ansible-playbook main.yml "$@"
+ANSIBLE_LOG_PATH="${LOG_PATH}" ansible-playbook main.yml "$@"
 
 # Lint the playbook
 ansible-lint main.yml
