ad869523bf08030c2ea757d0f6e95911e6ffe110
Mike Lim <choonchernlim@gmail.com>
2024-03-01 09:08:23 -0600

    moved ansible to pip instead of brew because it keeps breaking

--- mac-dev-bootstrap.sh
+++ mac-dev-bootstrap.sh
@@ -4,7 +4,8 @@ set -e -o pipefail
 readonly BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 readonly LOG_DIR="${BASE_DIR}/logs"
 readonly LOG_PATH="${LOG_DIR}/mac-dev-bootstrap-$(date +%Y%m%d%H%M%S).log"
-readonly PACKAGES="ansible ansible-lint homebrew/core/python"
+readonly PYTHON_VENV_DIR="${BASE_DIR}/.venv"
+readonly PACKAGES="homebrew/core/python"
 
 # Install Homebrew if missing, otherwise update all package definitions and Homebrew itself
 if [[ ! -x "$(command -v brew)" ]]; then
@@ -25,6 +26,17 @@ for package in ${PACKAGES}; do
   fi
 done
 
+# If .venv does not exist, create it and install ansible and ansible-lint
+if [[ ! -d "${PYTHON_VENV_DIR}" ]]; then
+  python3 -m venv "${PYTHON_VENV_DIR}"
+fi
+
+# Activate virtual environment
+source "${PYTHON_VENV_DIR}/bin/activate"
+
+# Pip install or upgrade ansible and ansible-lint
+python3 -m pip install --upgrade ansible ansible-lint
+
 # Homebrew installed Python in /usr/local for macOS Intel and /opt/homebrew for Apple Silicon
 readonly PYTHON_PATH=$(which python3)
 
@@ -34,8 +46,14 @@ mkdir -p "${LOG_DIR}"
 # Prompting for sudo password upfront (for handling `brew install --cask xxx`)
 sudo -v
 
+# Keep sudo alive until the script is done
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Run the playbook
 ANSIBLE_LOG_PATH="${LOG_PATH}" ANSIBLE_PYTHON_INTERPRETER="${PYTHON_PATH}" ansible-playbook main.yml "$@"
 
 # Lint the playbook
 ansible-lint
+
+# Deactivate virtual environment
+deactivate
